c9c74cea55b8901c6791e7bc8cec6a8d84bff572
coni-cp <c.parraguezs@duocuc.cl>
2025-05-16 01:44:36 -0400

    mover, desaparecer y creepy

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,31 @@
 console.log('conexion')
    
-$(document).ready(function(){
-    $(document).keydown(function(l) {
-     
-        const $personaje = $("#personaje");
-
-        if (l.which === 65){ // A - izquierda
-            $personaje
-                .css({ left: '-=20px' }, 100);
+$(document).ready(function () {
+     const $personaje = $("#personaje");
+    $(document).keydown(function (l) {
+        if (l.which === 65) { // A - izquierda
+            $personaje.stop(true, true).animate({ left: '-=20px' }, 100);
         } 
-        else if(l.which === 68) { // D - derecha
-            $personaje
-                .css({ left: '+=20px' }, 100);
+        else if (l.which === 68) { // D - derecha
+            $personaje.stop(true, true).animate({ left: '+=20px' }, 100);
         } 
         else if (l.which === 83) { // S - abajo
-            $personaje
-                .css({ top: '+=20px' }, 100);
+            $personaje.stop(true, true).animate({ top: '+=20px' }, 100);
         } 
-        else if(l.which === 87) { // W - arriba
-            $personaje
-                .css({ top: '-=20px' }, 100);
+        else if (l.which === 87) { // W - arriba
+            $personaje.stop(true, true).animate({ top: '-=20px' }, 100);
+        }
+        else if (l.which === 80) { // P- desaparece
+            $personaje.toggle(400);
         }
     });
+     setTimeout(function () {
+        $("#mata")
+            .show() // primero lo mostramos
+            .animate({ left: "300px" }, 2000); // luego lo deslizamos hacia la derecha
+    }, 8000);
+        
+
 });
+
+
