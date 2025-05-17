a638c85ccd3395fdb43bf80133cd7bcf167249c5
coni-cp <c.parraguezs@duocuc.cl>
2025-05-17 03:59:38 -0400

    solo falta el volver a comenzar

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,12 +27,41 @@ function start(){
             }
         });//funcion keydown
 
-        function moverMata() {
-            $("#mata")
-                .show()
-                .animate({ left: "300px" }, 3000)
-        }
-        setTimeout(moverMata, 10000);
+      function moverMata() {
+    const sonido = document.getElementById("audioExplosion");
+    const perdiste = document.getElementById("lastaudio");
+
+    $("#mata")
+        .show()
+        .animate({ left: "300px" }, 2000, function () {
+            $("#explosion").fadeIn(200); // Muestra la imagen
+
+            sonido.currentTime = 0;
+            sonido.play();
+
+            perdiste.currentTime = 0;
+            perdiste.play();
+
+           setTimeout(() => {
+                console.log("Mostrando mensaje final...");
+                $("#final").fadeIn(400);
+            }, 1500);
+
+        });
+}
+
+setTimeout(moverMata, 10000);
+
+
+
+
+      
+
+
+        
+      
+
+           
 
 
 
