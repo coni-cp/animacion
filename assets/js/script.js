94d006d462e295c9564a8be3ecb35f71b9ecfd15
coni-cp <c.parraguezs@duocuc.cl>
2025-05-17 18:53:43 -0400

    falta que hable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 console.log('conexion')
 
  document.getElementById('comenzar').addEventListener('click', function(){
- let ocultar = document.getElementById('ocultarIns')
- ocultar.style.display="none";
+    let ocultar = document.getElementById('ocultarIns');
+    ocultar.style.display="none";
 })
+
+$("#final").hide();
 
 
 function start(){
@@ -27,48 +29,25 @@ function start(){
             }
         });//funcion keydown
 
-      function moverMata() {
-        const sonido = document.getElementById("audioExplosion");
-        const perdiste = document.getElementById("lastaudio");
+        function aparece() {
+            const explosion = document.getElementById("audioExplosion");
+            const perdiste = document.getElementById("lastaudio");
 
-        $("#mata")
-            .show()
-            .animate({ left: "300px" }, 2000, function () {
-                $("#explosion").fadeIn(200); // Muestra la imagen
+            $("#mata")
+                .show()
+                .animate({ left: "300px" }, 2000, function () {
+                    explosion.play();
+                    $("#explosion").fadeIn(200);
 
-                sonido.currentTime = 0;
-                sonido.play();
+                setTimeout(() => {
+                        $("#final").fadeIn(400);
+                        perdiste.play();
+                    }, 1000);
 
-                perdiste.currentTime = 0;
-                perdiste.play();
+                });
+        }
 
-            setTimeout(() => {
-                    $("#final").fadeIn(400);
-                }, 1500);
-
-            });
-}
-
-setTimeout(moverMata, 10000);
-
-
-
-
-      
-
-
-        
-      
-
-           
-
-
-
-
-        
-
-
-        
+        setTimeout(aparece, 10000);
 
     });//funcion ready
 }//funcion start
