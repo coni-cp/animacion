d0a5974fe6282c08f82b093b3ae23c2af364831b
coni-cp <c.parraguezs@duocuc.cl>
2025-05-17 01:20:59 -0400

    timeout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,46 @@
 console.log('conexion')
-   
-$(document).ready(function () {
-     const $personaje = $("#personaje");
-    $(document).keydown(function (l) {
-        if (l.which === 65) { // A - izquierda
-            $personaje.stop(true, true).animate({ left: '-=20px' }, 100);
-        } 
-        else if (l.which === 68) { // D - derecha
-            $personaje.stop(true, true).animate({ left: '+=20px' }, 100);
-        } 
-        else if (l.which === 83) { // S - abajo
-            $personaje.stop(true, true).animate({ top: '+=20px' }, 100);
-        } 
-        else if (l.which === 87) { // W - arriba
-            $personaje.stop(true, true).animate({ top: '-=20px' }, 100);
+
+ document.getElementById('comenzar').addEventListener('click', function(){
+ let ocultar = document.getElementById('ocultarIns')
+ ocultar.style.display="none";
+})
+
+
+function start(){
+    $(document).ready(function () {
+        const $personaje = $("#personaje");
+        $(document).keydown(function (l) {
+            if (l.which === 65) { // A - izquierda
+                $personaje.stop(true, true).animate({ left: '-=20px' }, 100);
+            } 
+            else if (l.which === 68) { // D - derecha
+                $personaje.stop(true, true).animate({ left: '+=20px' }, 100);
+            } 
+            else if (l.which === 83) { // S - abajo
+                $personaje.stop(true, true).animate({ top: '+=20px' }, 100);
+            } 
+            else if (l.which === 87) { // W - arriba
+                $personaje.stop(true, true).animate({ top: '-=20px' }, 100);
+            }
+            else if (l.which === 80) { // P- desaparece
+                $personaje.toggle(400);
+            }
+        });//funcion keydown
+
+        function moverMata() {
+            $("#mata")
+                .show()
+                .animate({ left: "300px" }, 3000)
         }
-        else if (l.which === 80) { // P- desaparece
-            $personaje.toggle(400);
-        }
-    });
-     setTimeout(function () {
-        $("#mata")
-            .show() // primero lo mostramos
-            .animate({ left: "300px" }, 2000); // luego lo deslizamos hacia la derecha
-    }, 8000);
+        setTimeout(moverMata, 10000);
+
+
+
+
         
 
-});
 
+        
 
+    });//funcion ready
+}//funcion start
